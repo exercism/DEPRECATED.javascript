39f8d6e892d467f269b7b48f0c0344622cab9516
Wil Moore III <wil.moore@wilmoore.com>
2014-04-16 23:57:42 -0600

    refactor anagram example's API dropping constructor pattern (no new) and changing match to matches -- better responsibility separation also

--- anagram/anagram_test.spec.js
+++ anagram/anagram_test.spec.js
@@ -1,58 +1,68 @@
-var Anagram = require('./anagram');
+var anagram = require('./anagram');
 
 describe('Anagram', function() {
 
   it("no matches",function() {
-    var detector = new Anagram("diaper");
-    var matches = detector.match([ "hello", "world", "zombies", "pants"]);
+    var subject = anagram("diaper");
+    var matches = subject.matches([ "hello", "world", "zombies", "pants"]);
+
     expect(matches).toEqual([]);
   });
 
-  xit("detects simple anagram",function() {
-    var detector = new Anagram("ant");
-    var matches = detector.match(['tan', 'stand', 'at']);
+  it("detects simple anagram",function() {
+    var subject = anagram("ant");
+    var matches = subject.matches(['tan', 'stand', 'at']);
+
     expect(matches).toEqual(['tan']);
   });
 
-  xit("does not detect false positives",function() {
-    var detector = new Anagram("galea");
-    var matches = detector.match(["eagle"]);
+  it("does not detect false positives",function() {
+    var subject = anagram("galea");
+    var matches = subject.matches(["eagle"]);
+
     expect(matches).toEqual([]);
   });
 
-  xit("detects multiple anagrams",function() {
-    var detector = new Anagram("master");
-    var matches = detector.match(['stream', 'pigeon', 'maters']);
+  it("detects multiple anagrams",function() {
+    var subject = anagram("master");
+    var matches = subject.matches(['stream', 'pigeon', 'maters']);
+
     expect(matches).toEqual(['stream', 'maters']);
   });
 
-  xit("does not detect anagram subsets",function() {
-    var detector = new Anagram("good");
-    var matches = detector.match(['dog', 'goody']);
+  it("does not detect anagram subsets",function() {
+    var subject = anagram("good");
+    var matches = subject.matches(['dog', 'goody']);
+
     expect(matches).toEqual([]);
   });
 
-  xit("detects anagram",function() {
-    var detector = new Anagram("listen");
-    var matches = detector.match(['enlists', 'google', 'inlets', 'banana']);
+  it("detects anagram",function() {
+    var subject = anagram("listen");
+    var matches = subject.matches(['enlists', 'google', 'inlets', 'banana']);
+
     expect(matches).toEqual(['inlets']);
   });
 
-  xit("detects multiple anagrams",function() {
-    var detector = new Anagram("allergy");
-    var matches = detector.match(['gallery', 'ballerina', 'regally', 'clergy', 'largely', 'leading']);
+  it("detects multiple anagrams",function() {
+    var subject = anagram("allergy");
+    var matches = subject.matches(['gallery', 'ballerina', 'regally', 'clergy', 'largely', 'leading']);
+
     expect(matches).toEqual(['gallery', 'regally', 'largely']);
   });
 
-  xit("detects anagrams case-insensitively",function() {
-    var detector = new Anagram("Orchestra");
-    var matches = detector.match(['cashregister', 'Carthorse', 'radishes']);
+  it("detects anagrams case-insensitively",function() {
+    var subject = anagram("Orchestra");
+    var matches = subject.matches(['cashregister', 'Carthorse', 'radishes']);
+
     expect(matches).toEqual(['Carthorse']);
   });
 
-  xit("does not detect a word as its own anagram",function() {
-    var detector = new Anagram("banana");
-    var matches = detector.match(['Banana']);
+  it("does not detect a word as its own anagram",function() {
+    var subject = anagram("banana");
+    var matches = subject.matches(['Banana']);
+
     expect(matches).toEqual([]);
   });
+
 });
