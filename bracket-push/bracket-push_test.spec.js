9dd93715d592c3712a38e9000222df157661780d
Ginna <bakerv07@yahoo.com>
2014-08-27 10:58:50 -0400

    Refactored, added space, boolean literals, condensed test

--- bracket-push/bracket-push_test.spec.js
+++ bracket-push/bracket-push_test.spec.js
@@ -1,30 +1,28 @@
-describe("BracketPush()", function() {
+var bracket = require('./bracket-push');
+
+describe("bracketPush()", function() {
   it("checks for appropriate bracketing in a set of brackets", function() {
-    expect(BracketPush("{}")).toEqual("true");
+    expect(bracketPush("{}")).toEqual(true);
   });
-});
-describe("BracketPush()", function() {
-  it("returns false for unclosed brackets", function() {
-    expect(BracketPush("{{")).toEqual("false");
+
+  xit("returns false for unclosed brackets", function() {
+    expect(bracketPush("{{")).toEqual(false);
   });
-});
-describe("BracketPush()", function() {
-  it("can check bracketing in more than one pair of brackets", function() {
-    expect(BracketPush("{}[]")).toEqual("true");
+
+  xit("checks bracketing in more than one pair of brackets", function() {
+    expect(bracketPush("{}[]")).toEqual(true);
   });
-});
-describe("BracketPush()", function() {
-  it("can check bracketing in nested brackets", function() {
-    expect(BracketPush("{[]}")).toEqual("true");
+
+  xit("checks bracketing in nested brackets", function() {
+    expect(bracketPush("{[]}")).toEqual(true);
   });
-});
-describe("BracketPush()", function() {
-  it("checks bracket closure with deeper nesting", function() {
-    expect(BracketPush("{[)][]}")).toEqual("false");
+
+  xit("checks bracket closure with deeper nesting", function() {
+    expect(bracketPush("{[)][]}")).toEqual(false);
   });
-});
-describe("BracketPush()", function() {
-  it("checks bracket closure in a long string of brackets", function() {
-    expect(BracketPush("{[]([()])}")).toEqual("true");
+
+  xit("checks bracket closure in a long string of brackets", function() {
+    expect(bracketPush("{[]([()])}")).toEqual(true);
   });
+
 });
