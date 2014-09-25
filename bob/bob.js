206f348373eaf4ced24f6b11042ecc33cc3ccaac
David Schlesinger <lefty@shugendo.org>
2014-09-25 12:56:15 -0700

    Create bob.js

---
+++ bob/bob.js
@@ -0,0 +1,14 @@
+//
+// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+var Bob = function() {};
+
+Bob.prototype.hey = function(input) {
+//
+// YOUR CODE GOES HERE
+//
+};
+
+module.exports = Bob;
