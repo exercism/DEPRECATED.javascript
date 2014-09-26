b01478bb085e68228fea88e27cb2a61e9f9042f0
Colin <colinmorris@apple.com>
2014-09-25 18:19:14 -0700

    Expect naive whitespace-based tokenization in wordcount

--- word-count/word-count_test.spec.js
+++ word-count/word-count_test.spec.js
@@ -6,42 +6,37 @@ describe("words()", function() {
     expect(words("word")).toEqual(expectedCounts);
   });
 
-  xit("counts one of each", function() {
+  it("counts one of each", function() {
     var expectedCounts = { one: 1, of: 1, each: 1 };
     expect(words("one of each")).toEqual(expectedCounts);
   });
 
-  xit("counts multiple occurrences", function() {
+  it("counts multiple occurrences", function() {
     var expectedCounts = { one: 1, fish: 4, two: 1, red: 1, blue: 1 };
     expect(words("one fish two fish red fish blue fish")).toEqual(expectedCounts);
   });
 
-  xit("ignores punctuation", function() {
-    var expectedCounts = { car: 1, carpet: 1, as: 1, java: 1, javascript: 1 };
+  it("includes punctuation", function() {
+    var expectedCounts = { car: 1, ":": 2, carpet: 1, as: 1, java: 1, "javascript!!&@$%^&": 1 };
     expect(words("car : carpet as java : javascript!!&@$%^&")).toEqual(expectedCounts);
   });
 
-  xit("includes numbers", function() {
+  it("includes numbers", function() {
     var expectedCounts = { testing: 2, 1: 1, 2: 1 };
-    expect(words("testing, 1, 2 testing")).toEqual(expectedCounts);
+    expect(words("testing 1 2 testing")).toEqual(expectedCounts);
   });
 
-  xit("normalizes case", function() {
-    var expectedCounts = { go: 3 };
+  it("respects case", function() {
+    var expectedCounts = { go: 1, Go:1, GO:1 };
     expect(words("go Go GO")).toEqual(expectedCounts);
   });
 
-  xit("counts constructor", function() {
-    var expectedCounts = { constructor: 2 };
-    expect(words("constructor Constructor")).toEqual(expectedCounts);
-  });
-
-  xit("counts properly international characters", function() {
-    var expectedCounts = { hola: 1, "qué": 1, tal: 1, "привет": 1 };
+  it("counts properly international characters", function() {
+    var expectedCounts = { "¡Hola!": 1, "¿Qué": 1, "tal?": 1, "Привет!": 1 };
     expect(words("¡Hola! ¿Qué tal? Привет!")).toEqual(expectedCounts);
   });
 
-  xit("counts multiline", function() {
+  it("counts multiline", function() {
     var expectedCounts = { hello: 1, world: 1 };
     expect(words("hello\nworld")).toEqual(expectedCounts);
   });
