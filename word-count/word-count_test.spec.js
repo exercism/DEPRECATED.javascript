8cba53a644a8cfea403686cf2cab5ad602f1bd45
Colin <colinmorris@apple.com>
2014-09-25 18:21:10 -0700

    redisable all but first unit test from wordcount

--- word-count/word-count_test.spec.js
+++ word-count/word-count_test.spec.js
@@ -6,37 +6,37 @@ describe("words()", function() {
     expect(words("word")).toEqual(expectedCounts);
   });
 
-  it("counts one of each", function() {
+  xit("counts one of each", function() {
     var expectedCounts = { one: 1, of: 1, each: 1 };
     expect(words("one of each")).toEqual(expectedCounts);
   });
 
-  it("counts multiple occurrences", function() {
+  xit("counts multiple occurrences", function() {
     var expectedCounts = { one: 1, fish: 4, two: 1, red: 1, blue: 1 };
     expect(words("one fish two fish red fish blue fish")).toEqual(expectedCounts);
   });
 
-  it("includes punctuation", function() {
+  xit("includes punctuation", function() {
     var expectedCounts = { car: 1, ":": 2, carpet: 1, as: 1, java: 1, "javascript!!&@$%^&": 1 };
     expect(words("car : carpet as java : javascript!!&@$%^&")).toEqual(expectedCounts);
   });
 
-  it("includes numbers", function() {
+  xit("includes numbers", function() {
     var expectedCounts = { testing: 2, 1: 1, 2: 1 };
     expect(words("testing 1 2 testing")).toEqual(expectedCounts);
   });
 
-  it("respects case", function() {
+  xit("respects case", function() {
     var expectedCounts = { go: 1, Go:1, GO:1 };
     expect(words("go Go GO")).toEqual(expectedCounts);
   });
 
-  it("counts properly international characters", function() {
+  xit("counts properly international characters", function() {
     var expectedCounts = { "¡Hola!": 1, "¿Qué": 1, "tal?": 1, "Привет!": 1 };
     expect(words("¡Hola! ¿Qué tal? Привет!")).toEqual(expectedCounts);
   });
 
-  it("counts multiline", function() {
+  xit("counts multiline", function() {
     var expectedCounts = { hello: 1, world: 1 };
     expect(words("hello\nworld")).toEqual(expectedCounts);
   });
