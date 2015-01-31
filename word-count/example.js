ce7e239669912e0d70066ccc7283300e954a4066
James Costian <jamescostian@users.noreply.github.com>
2015-01-30 21:24:07 -0600

    Make word count example understand multiple spaces
    This commit was added to make the example.js file pass the tests in
    d18dc34ee0ab6cb17feee2f89c94626fcb455f9e

--- word-count/example.js
+++ word-count/example.js
@@ -2,7 +2,7 @@
 
 module.exports = function (input) {
   var counts = {};
-  var words = input.split(/\s/);
+  var words = input.split(/\s+/);
 
   words.forEach(function (word) {
     counts[word] = counts.hasOwnProperty(word) ? counts[word] + 1 : 1;
