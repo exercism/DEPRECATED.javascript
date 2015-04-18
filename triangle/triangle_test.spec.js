65eae28bf7e2cd0d576397c0211a4f828061e879
Rootul Patel <rootulp@gmail.com>
2015-04-18 18:16:57 -0400

    Comment out all triangle tests (except first one)
    Made change to conform to the style of previous xjavascript specs. Also
    having only one test uncommented suggests incremental development.

--- triangle/triangle_test.spec.js
+++ triangle/triangle_test.spec.js
@@ -7,72 +7,72 @@ describe("Triangle", function() {
     expect(triangle.kind()).toEqual("equilateral");
   });
 
-  it("larger equilateral triangles also have equal sides", function() {
+  xit("larger equilateral triangles also have equal sides", function() {
     var triangle = new Triangle(10,10,10);
     expect(triangle.kind()).toEqual("equilateral");
   });
 
-  it("isosceles triangles have last two sides equal", function() {
+  xit("isosceles triangles have last two sides equal", function() {
     var triangle = new Triangle(3,4,4);
     expect(triangle.kind()).toEqual("isosceles");
   });
 
-  it("isosceles trianges have first and last sides equal", function() {
+  xit("isosceles trianges have first and last sides equal", function() {
     var triangle = new Triangle(4,3,4);
     expect(triangle.kind()).toEqual("isosceles");
   });
 
-  it("isosceles triangles have two first sides equal", function() {
+  xit("isosceles triangles have two first sides equal", function() {
     var triangle = new Triangle(4,4,3);
     expect(triangle.kind()).toEqual("isosceles");
   });
 
-  it("isosceles triangles have in fact exactly two sides equal", function() {
+  xit("isosceles triangles have in fact exactly two sides equal", function() {
     var triangle = new Triangle(10,10,2);
     expect(triangle.kind()).toEqual("isosceles");
   });
 
-  it("scalene triangles have no equal sides", function() {
+  xit("scalene triangles have no equal sides", function() {
     var triangle = new Triangle(3,4,5);
     expect(triangle.kind()).toEqual("scalene");
   });
 
-  it("scalene triangles have no equal sides at a larger scale too", function() {
+  xit("scalene triangles have no equal sides at a larger scale too", function() {
     var triangle = new Triangle(10,11,12);
     expect(triangle.kind()).toEqual("scalene");
   });
 
-  it("scalene triangles have no equal sides in descending order either", function() {
+  xit("scalene triangles have no equal sides in descending order either", function() {
     var triangle = new Triangle(5,4,2);
     expect(triangle.kind()).toEqual("scalene");
   });
 
-  it("very small triangles are legal", function() {
+  xit("very small triangles are legal", function() {
     var triangle = new Triangle(0.4,0.6,0.3);
     expect(triangle.kind()).toEqual("scalene");
   });
 
-  it("test triangles with no size are illegal", function() {
+  xit("test triangles with no size are illegal", function() {
     var triangle = new Triangle(0,0,0);
     expect(triangle.kind.bind(triangle)).toThrow();
   });
 
-  it("triangles with negative sides are illegal", function() {
+  xit("triangles with negative sides are illegal", function() {
     var triangle = new Triangle(3,4,-5);
     expect(triangle.kind.bind(triangle)).toThrow();
   });
 
-  it("triangles violating triangle inequality are illegal", function() {
+  xit("triangles violating triangle inequality are illegal", function() {
     var triangle = new Triangle(1,1,3);
     expect(triangle.kind.bind(triangle)).toThrow();
   });
 
-  it("triangles violating triangle inequality are illegal 2", function() {
+  xit("triangles violating triangle inequality are illegal 2", function() {
     var triangle = new Triangle(2,4,2);
     expect(triangle.kind.bind(triangle)).toThrow();
   });
 
-  it("triangles violating triangle inequality are illegal 3", function() {
+  xit("triangles violating triangle inequality are illegal 3", function() {
     var triangle = new Triangle(7,3,2);
     expect(triangle.kind.bind(triangle)).toThrow();
   });
