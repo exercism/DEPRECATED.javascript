5fdbbd3d91d6b1e17888d5a855fce50deb6a38a4
Rootul Patel <rootulp@gmail.com>
2015-05-08 23:33:29 -0400

    Update gigasecond to measure milliseconds not dates
    This closes #73
    
    use Math.pow for readability
    
    Use Date.UTC to fix hour offset problem
    
    The Travis CI build failed because there was a discrepancy between local
    time and UTC time. These updated tests should solve the issue.

--- gigasecond/gigasecond_test.spec.js
+++ gigasecond/gigasecond_test.spec.js
@@ -3,20 +3,20 @@ var Gigasecond = require('./gigasecond');
 describe("Gigasecond", function() {
 
   it("test 1", function() {
-    var gs = new Gigasecond(new Date(2011, 3, 25));
-    var expectedDate = new Date(2043, 0, 1);
+    var gs = new Gigasecond(new Date(Date.UTC(2011, 3, 25)));
+    var expectedDate = new Date(Date.UTC(2043, 0, 1, 1, 46, 40));
     expect(gs.date()).toEqual(expectedDate);
   });
 
   xit("test 2", function() {
-    var gs = new Gigasecond(new Date(1977, 5, 13));
-    var expectedDate = new Date(2009, 1, 19);
+    var gs = new Gigasecond(new Date(Date.UTC(1977, 5, 13)));
+    var expectedDate = new Date(Date.UTC(2009, 1, 19, 1, 46, 40));
     expect(gs.date()).toEqual(expectedDate);
   });
 
   xit("test 3", function() {
-    var gs = new Gigasecond(new Date(1959, 6, 19));
-    var expectedDate = new Date(1991, 2, 27);
+    var gs = new Gigasecond(new Date(Date.UTC(1959, 6, 19)));
+    var expectedDate = new Date(Date.UTC(1991, 2, 27, 1, 46, 40));
     expect(gs.date()).toEqual(expectedDate);
   });
 
