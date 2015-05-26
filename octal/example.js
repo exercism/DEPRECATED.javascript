aedaad2f5c31dfb9589bd770bbe203496b09249e
Matthias Rahlf <matthias@webding.de>
2015-05-26 05:35:50 +0200

    added missing unit test

--- octal/example.js
+++ octal/example.js
@@ -3,13 +3,18 @@
 var BASE = 8;
 
 function Octal(octal) {
-  this.digits = octal.split('').reverse().map(Number);
+  this.digits = octal.split('').reverse().map(this.digitValue);
 }
 
 Octal.prototype.toDecimal = function() {
   var decimal = this.digits.reduce(this.accumulator, 0);
   return isNaN(decimal) ? 0 : decimal;
 };
+
+Octal.prototype.digitValue = function(value) {
+  var result = Number(value);
+  return (result < BASE)? result : Number.NaN;
+}
 
 Octal.prototype.accumulator = function(decimal, digit, index) {
   return decimal += digit * Math.pow(BASE, index);
