6658f00bc61aea592a525d33ae48eb1ea08de364
Katrina Owen <katrina.owen@gmail.com>
2015-08-01 10:44:52 -0600

    Add an extra hint in hello-world

--- hello-world/hello-world.js
+++ hello-world/hello-world.js
@@ -1,7 +1,8 @@
 //
 // This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
 // convenience to get you started writing code faster.
-//
+// Make sure to look at test.script.js--that should give you some hints about what is
+// expected here.
 
 var HelloWorld = function() {};
 
