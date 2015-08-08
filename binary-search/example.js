32e485e92a995a10d4b47e3e3bccc72e9a0b599c
NathanEpstein <ne2210@columbia.edu>
2015-08-08 11:45:48 -0400

    adding binary-search problem

---
+++ binary-search/example.js
@@ -0,0 +1,35 @@
+'use strict'
+
+function BinarySearch(array) {
+
+  //check if array is sorted
+  var arrayIsSorted = true;
+  for (var i = 1; i < array.length; i++) {
+    if (array[i] < array[i - 1]) arrayIsSorted = false;
+  }
+
+  //instantiate the array if sorted
+  if (arrayIsSorted) this.array = array;
+
+  //use binary search for indexOf
+  this.indexOf = function(value) {
+    return recursiveSearch(this.array, value, 0, this.array.length);
+  }
+}
+
+
+function recursiveSearch(array, value, start, end) {
+  if (start == end) return -1;
+
+  var mid = Math.floor((start + end) / 2);
+
+  if (array[mid] > value) {
+    return recursiveSearch(array, value, start, mid);
+  }
+  if (array[mid] < value) {
+    return recursiveSearch(array, value, mid + 1, end);
+  }
+  return mid;
+}
+
+module.exports = BinarySearch;
