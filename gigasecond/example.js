b984811bcd8b53a7845bb792a99f1c4b23a55fb9
Matthew Morgan <matt@dulcetsoftware.com>
2015-11-15 12:40:17 -0500

    fixed gigasecond example and test to conform to metadata-- dont round dates down

--- gigasecond/example.js
+++ gigasecond/example.js
@@ -5,14 +5,7 @@ function Gigasecond(dateOfBirth) {
 
   this.date = function() {
     var gigasecondDate = new Date(this.dateOfBirth.getTime() + 1000000000000);
-    return this.roundDownToDay(gigasecondDate);
-  };
-
-  this.roundDownToDay = function(date) {
-    date.setHours(0);
-    date.setMinutes(0);
-    date.setSeconds(0);
-    return date;
+    return gigasecondDate;
   };
 }
 
