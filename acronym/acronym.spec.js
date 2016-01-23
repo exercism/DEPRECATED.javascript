786ea113571f018e7e02907da429ed9dee5ba8a3
Matthew Morgan <matt@dulcetsoftware.com>
2016-01-23 17:24:14 -0500

    Add new exercise acronym

---
+++ acronym/acronym.spec.js
@@ -0,0 +1,28 @@
+var Acronyms = require('./acronym');
+
+describe('Acronyms are produced from', function(){
+  it('title cased phrases', function() {
+    expect(Acronyms.parse('Portable Network Graphics')).toEqual('PNG');
+  });
+
+  it('other title cased phrases', function(){
+    expect(Acronyms.parse('Ruby on Rails')).toEqual('ROR');
+  });
+
+  it('inconsistently cased phrases', function(){
+    expect(Acronyms.parse('HyperText Markup Language')).toEqual('HTML');
+  });
+
+  it('phrases with punctuation', function() {
+    expect(Acronyms.parse('First In, First Out')).toEqual('FIFO');
+  });
+
+  it('other phrases with punctuation', function() {
+    expect(Acronyms.parse('PHP: Hypertext Preprocessor')).toEqual('PHP');
+  });
+
+  it('phrases with punctuation and sentence casing', function() {
+    expect(Acronyms.parse('Complementary metal-oxide semiconductor')).toEqual('CMOS');
+  });
+});
+
---
+++ acronym/example.js
@@ -0,0 +1,8 @@
+module.exports = {
+  parse: function (phrase) {
+    return phrase.match(/[A-Z]+[a-z]*|[a-z]+/g)
+      .reduce(function (acronym, word) {
+        return acronym += word[0].toUpperCase()
+      }, '');
+  }
+};
