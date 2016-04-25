7ea190324166ec2da48016d66416d4047434a4ea
Yvan SRAKA <yvan.sraka@gmail.com>
2016-04-25 17:25:54 +0200

    Fix FizzBuzz exercise

--- exercises/fizzbuzz/README.md
+++ exercises/fizzbuzz/README.md
@@ -1,13 +1,13 @@
 # FizzBuzz
 
-* `Fork` the GitHub repository
+* `Fork` the GitHub repository.
 * `git add`, `git commit` and` git push` directly on your repository as much you want.
 * Open an `Pull request`, you must get a green light on the Travis CI server to validate the exercise!
 
 ## Rules
 
-* print out `"fizz"` if the integer is a multiple of `3` (and not a multiple of `5`)
-* print out `"buzz"` if the integer is a multiple of `5` (and not a multiple of `3`)
+* print `"fizz"` if the integer is a multiple of `3` (and not a multiple of `5`)
+* print `"buzz"` if the integer is a multiple of `5` (and not a multiple of `3`)
 * print `"fizzbuzz"` if the integer is a multiple of `3` and `5`
 * print the `number` if the integer is not a multiple of `3` or `5`
 
@@ -23,7 +23,7 @@ fizzbuzz(30) => "fizzbuzz"
 
 ## GO, GO, GO!
 
-May the force be with you
+May the force be with you :space_invader: !
 
 ## To go further..
 
--- exercises/fizzbuzz/fizzbuzz.spec.js
+++ exercises/fizzbuzz/fizzbuzz.spec.js
@@ -1,25 +1,29 @@
-var fizzbuzz = require('./fizzbuzz')();
+var fizzbuzz = require('./fizzbuzz');
 
 describe('fizzbuzz', function() {
-
-  it('should have one when one passed', function() {
-    var result = fizzbuzz.fizzbuzz(1);
-    expect(result).toBe('1');
+  
+  it('should have 2 when two passed', function() {
+    expect(fizzbuzz.fizzbuzz(2)).toBe(2);
   });
 
   it('should have "fizz" when three passed', function() {
-    var result = fizzbuzz.fizzbuzz(3);
-    expect(result).toBe('fizz');
+    expect(fizzbuzz.fizzbuzz(3)).toBe('fizz');
   });
 
   it('should have "buzz" when five passed', function() {
-    var result = fizzbuzz.fizzbuzz(5);
-    expect(result).toBe('buzz');
+    expect(fizzbuzz.fizzbuzz(5)).toBe('buzz');
   });
-
+  
+  it('should have "fizz" when six passed', function() {
+    expect(fizzbuzz.fizzbuzz(6)).toBe('fizz');
+  });
+  
+  it('should have 11 when eleven passed', function() {
+    expect(fizzbuzz.fizzbuzz(11)).toBe(11);
+  });
+  
   it('should have "fizzbuzz" when multiple of three and five passed', function() {
-    var result = fizzbuzz.fizzbuzz(15);
-    expect(result).toBe('fizzbuzz');
+    expect(fizzbuzz.fizzbuzz(30)).toBe('fizzbuzz');
   });
 
 });
