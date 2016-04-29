b5052a144abcfe2ff3f079081f38ccd611c0ec07
Katrina Owen <katrina.owen@gmail.com>
2016-04-29 17:07:13 -0600

    Clarify hello-world stub file comments
    I removed 'only' because it doesn't seem to serve a purpose.
    I switched SKELETON to stub because we don't need to shout, and we use the term stub file elsewhere
    on the site.
    Lastly, I changed the file name to match the actual file name in the exercise.

--- exercises/hello-world/hello-world.js
+++ exercises/hello-world/hello-world.js
@@ -1,7 +1,7 @@
 //
-// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
+// This is a stub file for the 'Hello World' exercise. It's been provided as a
 // convenience to get you started writing code faster.
-// Make sure to look at test.script.js--that should give you some hints about what is
+// Make sure to look at hello-world.spec.js--that should give you some hints about what is
 // expected here.
 
 var HelloWorld = function() {};
