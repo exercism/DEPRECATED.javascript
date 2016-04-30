9f505712c0328f58538e0e8bb2a72721d93c37d9
leonardo <cloudleo@leonardos-MacBook-Air.local>
2016-04-30 19:12:13 -0400

    simplified flatten-array

--- exercises/flatten-array/example.js
+++ exercises/flatten-array/example.js
@@ -1,23 +1,18 @@
-var Flattener = function(){
-  this.flattenedArray = [];
-};
+var Flattener = function(){};
 
 Flattener.prototype.flatten = function(unflattenedArray, flattenedArray) {
-
-   var self = this;
-    unflattenedArray.forEach(function(m){
-    if(Array.isArray(m)){
-     return self.flatten(m, self.flattenedArray);
-     }else if( m !== null){
-     self.flattenedArray.push(m);
+   var  self = this, flattenedArray = flattenedArray || []; 
+   unflattenedArray.forEach(function(m){
+       if(Array.isArray(m)){
+           return self.flatten(m, flattenedArray);
+       }else{
+           flattenedArray.push(m);
 }
   });
-  return self.flattenedArray;
+  return flattenedArray;
   }
 
-Flattener.prototype.clear = function(){
-  return this.flattenedArray = [];
-}
+
 ;
 
 module.exports = Flattener;
--- exercises/flatten-array/flatten-array.spec.js
+++ exercises/flatten-array/flatten-array.spec.js
@@ -2,25 +2,25 @@ var Flattener = require("./flatten-array.js");
 
 describe("FlattenArray", function() {
 	var flattener = new Flattener();
-
-	it('it clears the stored array', function(){
-		flattener.clear();
-		expect(flattener.clear()).toEqual([]);
+    it('flattens a nested list', function(){
+		expect(flattener.flatten([[]])).toEqual([]);
 	});
 
-    xit('flattens Array with null and numbers', function(){
-		flattener.clear();
-		expect(flattener.flatten([0, 2, [[2, 3], 8, 100, null, [[null]]], -2])).toEqual([0, 2, 2, 3, 8, 100, -2]);
+    xit('flattens a nested list', function(){
+		expect(flattener.flatten([1,[2,3,4],5])).toEqual([1, 2, 3, 4, 5]);
+	});
+
+    xit('flattens a nested list', function(){
+		expect(flattener.flatten([1,[2,3,4],5,[6,[7,8]]])).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+	});
+	xit('flattens Array with null and numbers', function(){
+		expect(flattener.flatten([0, 2, [[2, 3], 8, 100, 4,[[[50]]]], -2])).toEqual([0, 2, 2, 3, 8, 100, 4, 50, -2]);
 	});
     
     xit('flattens Array with just numbers', function(){
-		flattener.clear();
-		expect(flattener.flatten([1,[2,3,4,5,6,7],8])).toEqual([1,2,3,4,5,6,7,8]);
+		expect(flattener.flatten([1,[2,3,[4,[[5]]],6,7],8])).toEqual([1,2,3,4,5,6,7,8]);
 	});
 	
-	xit('flattens Array and it doesnt take null', function(){
-		flattener.clear();
-		expect(flattener.flatten([null,[[[null]]],null,null,[[null,null],null],null])).toEqual([]);
-	});  
+	
 
 });
