9f81d4adcc7491264248842e2e73e6b8b5aaebf3
leonardo <cloudleo@leonardos-MacBook-Air.local>
2016-04-30 16:46:22 -0400

    adding flatten-array to the list with test

---
+++ exercises/flatten-array/example.js
@@ -0,0 +1,15 @@
+var Flattener = function(){};
+
+Flattener.prototype.flatten = function(unflattenedArray, flattenedArray){
+  var  self = this, flattenedArray = flattenedArray || [];
+    unflattenedArray.forEach(function(element){
+      if(Array.isArray(element)){
+        return self.flatten(element, flattenedArray);
+      }else if( element !== null){
+        flattenedArray.push(element);
+      }
+    });
+   return flattenedArray;
+}
+
+module.exports = Flattener;
