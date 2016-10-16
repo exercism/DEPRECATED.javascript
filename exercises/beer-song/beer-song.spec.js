000b19f0760be0e37284e079899c9091fb8095b8
Lauren Nemtsev <lauren.nemtsev@gmail.com>
2016-10-16 17:17:09 -0500

    beer-song: Add test case for 2 bottles.

--- exercises/beer-song/beer-song.spec.js
+++ exercises/beer-song/beer-song.spec.js
@@ -8,6 +8,11 @@ describe('BeerSong', function() {
     expect(song.verse(8)).toEqual(expected);
   });
 
+  xit('handles 2 bottles', function() {
+    var expected = '2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n';
+    expect(song.verse(2)).toEqual(expected);
+  });
+
   xit('handles 1 bottle', function() {
     var expected = '1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n';
     expect(song.verse(1)).toEqual(expected);
