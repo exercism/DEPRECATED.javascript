8f30a79c4d8c4420876b884339ea2d46111b01ac
ldwoolley <ldwoolley@gmail.com>
2016-12-04 16:36:32 -0800

    Update word-count in xjavascript to match the x-common reference.  The test now checks for the elimination of punctuation.

--- exercises/word-count/example.js
+++ exercises/word-count/example.js
@@ -4,15 +4,15 @@ function Words() {}
 
 Words.prototype.count = function (input) {
   var counts = {};
-  var words = input.match(/\S+/g);
+  var words = input.toLowerCase()
+                   .replace(/[,."\/!&@$%\^\*;:{}()¡¿?]/g, ' ')
+                   .replace(/\s'(\w+)'\s/, ' '+'$1'+' ')
+                   .match(/\S+/g);
 
   words.forEach(function (word) {
-    var lcWord = word.toLowerCase();
-    counts[lcWord] = counts.hasOwnProperty(lcWord) ? counts[lcWord] + 1 : 1;
+    counts[word] = counts.hasOwnProperty(word) ? counts[word] + 1 : 1;
   });
-
   return counts;
 };
 
 module.exports = Words;
-
