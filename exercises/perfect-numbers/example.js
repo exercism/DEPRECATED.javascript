ef8714549f0a42fe136be5df1694a8b797e750c6
Felipe Armoni <felipe.armoni@gmail.com>
2017-03-21 13:21:44 -0300

    Add exercise perfect-numbers (#332)
    * Implement exercise perfect-numbers
    
    * Implemented perfect-numbers exercise.
    
    * Added semicolons and curly braces.
    
    * Removed the divisors module.
    
    * Updated config.json with the 'perfect-numbers' exercise info.
    
    * Updated config.json. Solved typo 'prefect-numbers' instead of 'perfect-numbers'
    
    * Updated config.json - Reordered perfect-numbers exercise.

---
+++ exercises/perfect-numbers/example.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var PerfectNumbers = function() {
+
+};
+
+/**
+ * Calculate all the divisors for a given number and return them as an array.
+ * Note: the actual number is not include in the returned array.
+ */
+PerfectNumbers.prototype.getDivisors = function(number) {
+
+  var i;
+  var divs = new Array();
+
+  // Accepts only natura numbers greater than 1.
+  if (number <= 1) {
+    return divs;
+	}
+
+  // 1 always divides everyone!
+  divs.push(1);
+
+  // Calculate the divisors up the the half of the number + 1
+  for (i = 2; i <= number / 2; i++) {
+
+    if (number % i === 0) {
+      divs.push(i);
+		}
+  }
+
+  return divs;
+};
+
+PerfectNumbers.prototype.classify = function(number) {
+
+  var i, sum, result;
+
+  // Check if the input is valid
+  if (number <= 0) {
+    return 'Classification is only possible for natural numbers.';
+  }
+
+  // Factorize the current number.
+  var divsArray = this.getDivisors(number);
+
+  // Sum the factors.
+  sum = 0;
+  for (i = 0; i < divsArray.length; i++) {
+    sum = sum + divsArray[i];
+  }
+
+  // Check if the number is perfect.
+  if (sum === number) {
+    result = 'perfect';
+  }
+  else if (sum > number) {
+    result = 'abundant';
+  }
+  else {
+    result = 'deficient';
+  }
+
+  return result;
+};
+
+module.exports = PerfectNumbers;
---
+++ exercises/perfect-numbers/perfect-numbers.spec.js
@@ -0,0 +1,79 @@
+var PerfectNumbers = require('./perfect-numbers');
+
+describe('Exercise - Perfect Numbers', function() {
+
+  var perfectNumbers;
+
+  beforeEach(function () {
+    perfectNumbers = new PerfectNumbers();
+  });
+
+  describe('Perfect Numbers', function() {
+
+    it('Smallest perfect number is classified correctly', function() {
+      expect(perfectNumbers.classify(6)).toEqual('perfect');
+    });
+
+    it('Medium perfect number is classified correctly', function() {
+      expect(perfectNumbers.classify(28)).toEqual('perfect');
+    });
+
+    it('Large perfect number is classified correctly', function() {
+      expect(perfectNumbers.classify(33550336)).toEqual('perfect');
+    });
+
+  });
+
+  describe('Abundant Numbers', function() {
+
+    it('Smallest abundant number is classified correctly', function() {
+      expect(perfectNumbers.classify(12)).toEqual('abundant');
+    });
+
+    it('Medium abundant number is classified correctly', function() {
+      expect(perfectNumbers.classify(30)).toEqual('abundant');
+    });
+
+    it('Large abundant number is classified correctly', function() {
+      expect(perfectNumbers.classify(33550335)).toEqual('abundant');
+    });
+
+  });
+
+  describe('Deficient Numbers', function() {
+
+    it('Smallest prime deficient number is classified correctly', function() {
+      expect(perfectNumbers.classify(2)).toEqual('deficient');
+    });
+
+    it('Smallest non-prime deficient number is classified correctly', function() {
+      expect(perfectNumbers.classify(4)).toEqual('deficient');
+    });
+
+    it('Medium deficient number is classified correctly', function() {
+      expect(perfectNumbers.classify(32)).toEqual('deficient');
+    });
+
+    it('Large deficient number is classified correctly', function() {
+      expect(perfectNumbers.classify(33550337)).toEqual('deficient');
+    });
+
+    it('Edge case (no factors other than itself) is classified correctly', function() {
+      expect(perfectNumbers.classify(1)).toEqual('deficient');
+    });
+
+  });
+
+  describe('Invalid Inputs', function() {
+
+    it('Zero is rejected (not a natural number)', function() {
+      expect(perfectNumbers.classify(0)).toEqual('Classification is only possible for natural numbers.');
+    });
+
+    it('Negative integer is rejected (not a natural number)', function() {
+      expect(perfectNumbers.classify(-1)).toEqual('Classification is only possible for natural numbers.');
+    });
+
+  });
+
+});
