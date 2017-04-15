340f400e7b0e53601d57ca81ccb6028a5e64bbdd
Joel Wallis Jucá <joelwallis@gmail.com>
2017-04-15 20:24:47 -0300

    Uses a function instead of constructor in Leap exercise
    See #337

--- exercises/leap/leap.spec.js
+++ exercises/leap/leap.spec.js
@@ -1,48 +1,40 @@
-var Year = require('./leap');
+var isLeap = require('./leap');
 
 describe('Leap year', function() {
 
   it('is not very common', function() {
-    var year = new Year(2015);
-    expect(year.isLeap()).toBe(false);
+    expect(isLeap(2015)).toBe(false);
   });
 
   xit('is introduced every 4 years to adjust about a day', function() {
-    var year = new Year(2016);
-    expect(year.isLeap()).toBe(true);
+    expect(isLeap(2016)).toBe(true);
   });
 
   xit('is skipped every 100 years to remove an extra day', function() {
-    var year = new Year(1900);
-    expect(year.isLeap()).toBe(false);
+    expect(isLeap(1900)).toBe(false);
   });
 
   xit('is reintroduced every 400 years to adjust another day', function() {
-    var year = new Year(2000);
-    expect(year.isLeap()).toBe(true);
+    expect(isLeap(2000)).toBe(true);
   });
 
   // Feel free to enable the following tests to check some more examples
   xdescribe('Additional example of a leap year that', function () {
 
     it('is not a leap year', function () {
-      var year = new Year(1978);
-      expect(year.isLeap()).toBe(false);
+      expect(isLeap(1978)).toBe(false);
     });
 
     it('is a common leap year', function () {
-      var year = new Year(1992);
-      expect(year.isLeap()).toBe(true);
+      expect(isLeap(1992)).toBe(true);
     });
 
     it('is skipped every 100 years', function () {
-      var year = new Year(2100);
-      expect(year.isLeap()).toBe(false);
+      expect(isLeap(2100)).toBe(false);
     });
 
     it('is reintroduced every 400 years', function () {
-      var year = new Year(2400);
-      expect(year.isLeap()).toBe(true);
+      expect(isLeap(2400)).toBe(true);
     });
 
   });
