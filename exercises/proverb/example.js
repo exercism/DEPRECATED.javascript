90a3966fc4fd465c23c93871d9c9bafce90d1e5d
Maël ARNAUD <mael.arnaud@gmail.com>
2017-04-27 14:36:13 +0200

    Adds jasmine specs for proverb exercise
    Adds example solution for proverb exercise
    
    Adds proverb exercise to the config file
    
    Makes the code emphasizes ES5

---
+++ exercises/proverb/example.js
@@ -0,0 +1,36 @@
+module.exports = function(){
+  var last = arguments[arguments.length - 1];
+  var chain = Array.from(arguments);
+  this.options = {};
+
+  if(typeof last == "object" && last.hasOwnProperty("qualifier")){
+    this.options = chain.pop();
+  }
+
+  this.chain = chain;
+  this.qualifier = this.options.qualifier ? this.options.qualifier + ' ' : '';
+
+  this.toString = function(){
+    return this.chainOfEvents() + this.conclusion();
+  }.bind(this);
+
+  this.chainOfEvents = function(){
+    return this.causesAndEffects().map( function(entry){
+      return "For want of a " + entry.cause +
+        " the "  + entry.effect + " was lost.\n";
+    }.bind(this) ).join('');
+  }.bind(this);
+
+  this.causesAndEffects = function(){
+     return this.chain.reduce( function(array, event, index){
+      if(index < this.chain.length - 1){
+        array.push({ cause: event, effect: this.chain[index + 1] });
+      }
+      return array;
+    }.bind(this), [] );
+  }.bind(this);
+
+  this.conclusion = function(){
+    return "And all for the want of a " + this.qualifier + this.chain[0] + ".";
+  }.bind(this);
+};
