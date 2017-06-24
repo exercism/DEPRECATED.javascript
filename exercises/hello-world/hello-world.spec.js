c363e9af93b7c3117b99f00ed7ce17af4abcd5e3
KamilMalek <k.malekb@gmail.com>
2017-06-24 11:15:21 +0200

    Simplified the hello-world exercise

--- exercises/hello-world/hello-world.spec.js
+++ exercises/hello-world/hello-world.spec.js
@@ -3,15 +3,7 @@ var HelloWorld = require('./hello-world');
 describe('Hello World', function() {
   var helloWorld = new HelloWorld();
 
-  it('says hello world with no name', function() {
-    expect(helloWorld.hello('')).toEqual('Hello, World!');
-  });
-
-  xit('says hello to bob', function() {
-    expect(helloWorld.hello('Bob')).toEqual('Hello, Bob!');
-  });
-
-  xit('says hello to sally', function() {
-    expect(helloWorld.hello('Sally')).toEqual('Hello, Sally!');
+  it('says hello world', function() {
+    expect(helloWorld.hello()).toEqual('Hello, World!');
   });
 });
