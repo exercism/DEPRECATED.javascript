1444aa38d6e223bd34fec9b446f84b88d8fa1b68
vivshaw <hvivianshaw@gmail.com>
2017-07-03 03:51:46 -0400

    Implement exercise list-ops, squashing 11 commits:
    * begin writing tests
    * begin writing example.js
    * finish implementing spec & example
    * refactor tests into groups, reinstate xit, and lint
    * lint example.js
    * add list-ops exercise to config.json
    * use a saner module.exports statement, refactor accordingly
    * fix missing newline at eof
    * remove es6 features, move to instance methods, and add test suite to catch native Array function use
    * relint
    * re-disable tests after the first one...

---
+++ exercises/list-ops/example.js
@@ -0,0 +1,76 @@
+'use strict';
+
+function List(arr) {
+  this.values = arr || [];
+}
+
+List.prototype = {
+  append: function (otherList) {
+    var appended = this.values;
+
+    for (var i = 0; i < otherList.length(); i++) {
+      appended.push(otherList.values[i]);
+    }
+
+    return new List(appended);
+  },
+
+  concat: function (otherList) {
+    return this.append(otherList);
+  },
+
+  cons: function (item, arr) {
+    var x = new List([item]);
+    var xs = new List(arr)
+    return x.append(xs).values;
+  },
+
+  foldl: function (func, start) {
+    var acc = start;
+
+    for (var i = 0; i < this.length(); i++) {
+      acc = func(this.values[i], acc);
+    }
+
+    return acc;
+  },
+
+  foldr: function (func, start) {
+    var acc = start;
+
+    for (var i = this.length() - 1; i >= 0; i--) {
+      acc = func(this.values[i], acc);
+    }
+
+    return acc;
+  },
+
+  length: function () {
+    var count = 0;
+    this.values.forEach(function () { count++; });
+
+    return count;
+  },
+
+  reverse: function () {
+    return new List(this.foldl(this.cons, []));
+  },
+
+  map: function (func, arr) {
+    var applyFuncThenCons = function (x, acc) {
+      return this.cons(func(x), acc);
+    }
+
+    return new List(this.foldr(applyFuncThenCons.bind(this), []));
+  },
+
+  filter: function (pred) {
+    var consIfPred = function (x, acc) {
+      return pred(x) ? this.cons(x, acc) : acc;
+    };
+
+    return new List(this.foldr(consIfPred.bind(this), []));
+  }
+}
+
+module.exports = List;
