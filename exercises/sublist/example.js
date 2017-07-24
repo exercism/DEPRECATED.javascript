3472f176e988012840418e7bc9551d8ca45c35ea
Matthew Morgan <matt@dulcetsoftware.com>
2017-07-24 06:52:12 -0400

    Add exercise sublist

---
+++ exercises/sublist/example.js
@@ -0,0 +1,31 @@
+function List(list) {
+  this.list = list || [];
+
+  return {
+    list: this.list,
+    compare: function(other){
+        return {
+          '-1': isSublist(other.list, this.list)
+                  ? 'SUBLIST'
+                  : 'UNEQUAL',
+          '0':  isSublist(other.list, this.list)
+                  ? 'EQUAL'
+                  : 'UNEQUAL',
+          '1':  isSublist(this.list, other.list)
+                  ? 'SUPERLIST'
+                  : 'UNEQUAL'
+        }[lengthDiff(this, other)];
+    }
+  }
+}
+
+function lengthDiff(one, two){
+  return String(Math.sign(one.list.length - two.list.length));
+}
+
+function isSublist(one, two){
+  return one.join().match(two.join());
+}
+
+
+module.exports = List;
---
+++ exercises/sublist/sublist.spec.js
@@ -0,0 +1,126 @@
+var List = require('./sublist');
+
+
+describe('sublist', function() {
+
+  it('two empty lists are equal', function() {
+    var listOne = new List();
+    var listTwo = new List();
+
+    expect(listOne.compare(listTwo)).toEqual('EQUAL');
+  });
+
+  xit('an empty list is a sublist of a non-empty list', function() {
+    var listOne = new List();
+    var listTwo = new List([1, 2, 3]);
+
+    expect(listOne.compare(listTwo)).toEqual('SUBLIST');
+  });
+
+  xit('non empty list contains empty list', function() {
+    var listOne = new List([1, 2, 3]);
+    var listTwo = new List();
+
+    expect(listOne.compare(listTwo)).toEqual('SUPERLIST');
+  });
+
+  xit('a non-empty list equals itself', function() {
+    var listOne = new List([1, 2, 3]);
+    var listTwo = new List([1, 2, 3]);
+
+    expect(listOne.compare(listTwo)).toEqual('EQUAL');
+  });
+
+  xit('two different lists are unequal', function() {
+    var listOne = new List([1, 2, 3]);
+    var listTwo = new List([2, 3, 4]);
+
+    expect(listOne.compare(listTwo)).toEqual('UNEQUAL');
+  });
+
+  xit('false start', function() {
+    var listOne = new List([1, 2, 5]);
+    var listTwo = new List([0, 1, 2, 3, 1, 2, 5, 6]);
+
+    expect(listOne.compare(listTwo)).toEqual('SUBLIST');
+
+  });
+
+  xit('consecutive', function() {
+    var listOne = new List([1, 1, 2]);
+    var listTwo = new List([0, 1, 1, 1, 2, 1, 2]);
+
+    expect(listOne.compare(listTwo)).toEqual('SUBLIST');
+  });
+
+  xit('sublist at start', function() {
+    var listOne = new List([0, 1, 2]);
+    var listTwo = new List([0, 1, 2, 3, 4, 5]);
+
+    expect(listOne.compare(listTwo)).toEqual('SUBLIST');
+  });
+
+  xit('sublist in middle', function() {
+    var listOne = new List([2, 3, 4]);
+    var listTwo = new List([0, 1, 2, 3, 4, 5]);
+
+    expect(listOne.compare(listTwo)).toEqual('SUBLIST');
+  });
+
+  xit('sublist at end', function() {
+    var listOne = new List([3, 4, 5]);
+    var listTwo = new List([0, 1, 2, 3, 4, 5]);
+
+    expect(listOne.compare(listTwo)).toEqual('SUBLIST');
+  });
+
+  xit('at start of superlist', function() {
+    var listOne = new List([0, 1, 2, 3, 4, 5]);
+    var listTwo = new List([0, 1, 2]);
+
+    expect(listOne.compare(listTwo)).toEqual('SUPERLIST');
+  });
+
+  xit('in middle of superlist', function() {
+    var listOne = new List([0, 1, 2, 3, 4, 5]);
+    var listTwo = new List([2, 3]);
+
+    expect(listOne.compare(listTwo)).toEqual('SUPERLIST');
+  });
+
+  xit('at end of superlist', function() {
+    var listOne = new List([0, 1, 2, 3, 4, 5]);
+    var listTwo = new List([3, 4, 5]);
+
+    expect(listOne.compare(listTwo)).toEqual('SUPERLIST');
+  });
+
+  xit('first list missing element from second list', function() {
+    var listOne = new List([1, 3]);
+    var listTwo = new List([1, 2, 3]);
+
+    expect(listOne.compare(listTwo)).toEqual('UNEQUAL');
+  });
+
+  xit('second list missing element from first list', function() {
+    var listOne = new List([1, 2, 3]);
+    var listTwo = new List([1, 3]);
+
+    expect(listOne.compare(listTwo)).toEqual('UNEQUAL');
+  });
+
+  xit('order matters to a list', function() {
+    var listOne = new List([1, 2, 3]);
+    var listTwo = new List([3, 2, 1]);
+
+    expect(listOne.compare(listTwo)).toEqual('UNEQUAL');
+  });
+
+  xit('same digits but different numbers', function() {
+    var listOne = new List([1, 0, 1]);
+    var listTwo = new List([10, 1]);
+
+    expect(listOne.compare(listTwo)).toEqual('UNEQUAL');
+  });
+
+});
