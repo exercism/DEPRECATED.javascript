785994f261c50fc51a715c1d600cd1394d03b713
Rootul Patel <rootulp@palantir.com>
2017-10-01 18:47:57 +0100

    Rename variables to sane names

--- exercises/two-bucket/example.js
+++ exercises/two-bucket/example.js
@@ -1,14 +1,15 @@
 'use strict';
 
-function TwoBucket(x, y, z, starter) {
-  this.starter = starter;
-  this.x = x;
-  this.y = y;
+function TwoBucket(bucketOne, bucketTwo, goal, startBucket) {
+  this.startBucket = startBucket;
+  this.bucketOne = bucketOne;
+  this.bucketTwo = bucketTwo;
+  this.goal = goal;
 
   this.reachedGoal = function (measurements) {
     var reached = false;
-    if (measurements[0] == z || measurements[1] == z) {
-      if (measurements[0] == z) {
+    if (measurements[0] === goal || measurements[1] === goal) {
+      if (measurements[0] === goal) {
         this.goalBucket = 'one';
         this.otherBucket = measurements[1];
       } else {
@@ -21,22 +22,23 @@ function TwoBucket(x, y, z, starter) {
   };
 
   this.bigFirst = function (measurements, moveCount, prBool) {
-    var j = measurements[0], k = measurements[1];
+    var j = measurements[0];
+    var k = measurements[1];
     while (true) {
       if (this.reachedGoal(measurements)) break;
-      if (k > x && j == 0 && moveCount == 0) {
-        j = x;
-        k = y - j;
-      } else if (j == x) {
+      if (k > bucketOne && j === 0 && moveCount === 0) {
+        j = bucketOne;
+        k = bucketTwo - j;
+      } else if (j === bucketOne) {
         j = 0;
-      } else if ((k > x && j !== 0) || (k > x && prBool)) {
-        k = k - (x - j);
-        j = x;
-      } else if (k > x || j == 0) {
+      } else if ((k > bucketOne && j !== 0) || (k > bucketOne && prBool)) {
+        k = k - (bucketOne - j);
+        j = bucketOne;
+      } else if (k > bucketOne || j === 0) {
         j = k;
         k = k - j;
-      } else if (k == 0) {
-        k = y;
+      } else if (k === 0) {
+        k = bucketTwo;
       }
       measurements = [j, k];
       moveCount++;
@@ -46,21 +48,22 @@ function TwoBucket(x, y, z, starter) {
   };
 
   this.smallFirst = function (measurements, moveCount, prBool) {
-    var j = measurements[0], k = measurements[1];
+    var j = measurements[0];
+    var k = measurements[1];
     while (true) {
       if (this.reachedGoal(measurements)) break;
-      if (j == x && moveCount == 0) {
+      if (j === bucketOne && moveCount === 0) {
         j = 0;
-        k = x;
-      } else if (j == 0) {
-        j = x;
-      } else if (j == x && k < y) {
+        k = bucketOne;
+      } else if (j === 0) {
+        j = bucketOne;
+      } else if (j === bucketOne && k < bucketTwo) {
         var tempK = k;
-        k + j > y ? k = y : k = tempK + j;
-        tempK + j > y ? j = j - (y - tempK) : j = 0;
-      } else if (k == y) {
+        k + j > bucketTwo ? k = bucketTwo : k = tempK + j;
+        tempK + j > bucketTwo ? j = j - (bucketTwo - tempK) : j = 0;
+      } else if (k === bucketTwo) {
         k = 0;
-      } else if (k == 0 && j < x) {
+      } else if (k === 0 && j < bucketOne) {
         k = j;
         j = 0;
       }
@@ -73,17 +76,18 @@ function TwoBucket(x, y, z, starter) {
 }
 
 TwoBucket.prototype.moves = function () {
-  var j = 0, k = 0; // j will be running val of bucket one, k = running val of bucket two
-  this.starter == 'one' ? j = this.x : k = this.y;
+  var j = 0; // j will be running val of bucket one,
+  var k = 0; // k will be running val of bucket two
+  this.startBucket === 'one' ? j = this.bucketOne : k = this.bucketTwo;
   var measurements = [j, k];
   var moveCount = 0;
-  var prBool = true; // pour / receive boolean - need to pour or receive every other turn
-  if (this.starter == 'one') {
+  var prBool = true; // pour / receive boolean - need to pour or receive everbucketTwo other turn
+  if (this.startBucket === 'one') {
     moveCount = this.smallFirst(measurements, moveCount, prBool);
   } else {
     moveCount = this.bigFirst(measurements, moveCount, prBool);
   }
-  return moveCount + 1; // accounts for first move made before loop (and moveCount starts at zero before loop)
+  return moveCount + 1; // accounts for first move made before loop (and moveCount starts at goalero before loop)
 };
 
 module.exports = TwoBucket;
