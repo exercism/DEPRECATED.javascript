f852472a931f8c1e9ccdcbc0fc57ff4f879be2c8
krishna <krishwebz@gmail.com>
2017-10-15 22:00:46 +0530

    Update code and styles

--- exercises/transpose/example.js
+++ exercises/transpose/example.js
@@ -1,15 +1,17 @@
 'use strict';
 
 function transpose(text) {
-  return text.split('\n').reduce(function (result, line, lineNo) {
+  return text.reduce(function (result, line, lineNo) {
     line.split('').map(function (value, key) {
       if (typeof result[key] === 'undefined') {
-        result[key] = new Array(lineNo + 1).join( ' ' );
+        result[key] = new Array(lineNo + 1).join(' ');
       }
+
       result[key] += value;
     });
+
     return result;
-  }, []).join('\n');
+  }, []);
 }
 
 module.exports = transpose;
--- exercises/transpose/transpose.spec.js
+++ exercises/transpose/transpose.spec.js
@@ -2,64 +2,66 @@ var transpose = require('./transpose');
 
 describe('Transpose', function () {
   it('test empty string', function () {
-    expect(transpose('')).toEqual('');
+    expect(transpose([])).toEqual([]);
   });
 
   xit('test two characters in a row', function () {
-    var input = 'A1';
-    var expected = 'A\n1';
+    var input = ['A1'];
+    var expected = ['A', '1'];
     expect(transpose(input)).toEqual(expected);
   });
 
   xit('test two characters in a column', function () {
-    var input = 'A\n1';
-    var expected = 'A1';
+    var input = ['A', '1'];
+    var expected = ['A1'];
     expect(transpose(input)).toEqual(expected);
   });
 
   xit('test simple', function () {
-    var input = 'ABC\n123';
-    var expected = 'A1\nB2\nC3';
+    var input = ['ABC', '123'];
+    var expected = ['A1', 'B2', 'C3'];
     expect(transpose(input)).toEqual(expected);
   });
 
   xit('test single line', function () {
-    var input = 'Single line.';
-    var expected = 'S\ni\nn\ng\nl\ne\n \nl\ni\nn\ne\n.';
+    var input = ['Single line.'];
+    var expected = ['S', 'i', 'n', 'g', 'l', 'e', ' ', 'l', 'i', 'n', 'e', '.'];
     expect(transpose(input)).toEqual(expected);
   });
 
   xit('test first line longer than second line', function () {
-    var input = 'The fourth line.\nThe fifth line.';
-    var expected = 'TT\nhh\nee\n  \nff\noi\nuf\nrt\nth\nh \n l\nli\nin\nne\ne.\n.';
+    var input = ['The fourth line.', 'The fifth line.'];
+    var expected = ['TT', 'hh', 'ee', '  ', 'ff', 'oi', 'uf', 'rt', 'th', 'h ', ' l', 'li', 'in', 'ne', 'e.', '.'];
     expect(transpose(input)).toEqual(expected);
   });
 
   xit('test second line longer than first line', function () {
-    var input = 'The first line.\nThe second line.';
-    var expected = 'TT\nhh\nee\n  \nfs\nie\nrc\nso\ntn\n d\nl \nil\nni\nen\n.e\n .';
+    var input = ['The first line.', 'The second line.'];
+    var expected = ['TT', 'hh', 'ee', '  ', 'fs', 'ie', 'rc', 'so', 'tn', ' d', 'l ', 'il', 'ni', 'en', '.e', ' .'];
     expect(transpose(input)).toEqual(expected);
   });
 
   xit('test square', function () {
-    var input =  'HEART\nEMBER\nABUSE\nRESIN\nTREND';
-    var expected = 'HEART\nEMBER\nABUSE\nRESIN\nTREND';
+    var input =  ['HEART', 'EMBER', 'ABUSE', 'RESIN', 'TREND'];
+    var expected = ['HEART', 'EMBER', 'ABUSE', 'RESIN', 'TREND'];
     expect(transpose(input)).toEqual(expected);
   });
 
   xit('test rectangle', function () {
-    var input = 'FRACTURE\nOUTLINED\nBLOOMING\nSEPTETTE';
-    var expected = 'FOBS\nRULE\nATOP\nCLOT\nTIME\nUNIT\nRENT\nEDGE';
+    var input = ['FRACTURE', 'OUTLINED', 'BLOOMING', 'SEPTETTE'];
+    var expected = ['FOBS', 'RULE', 'ATOP', 'CLOT', 'TIME', 'UNIT', 'RENT', 'EDGE'];
     expect(transpose(input)).toEqual(expected);
   });
+
   xit('test triangle', function () {
-    var input = 'T\nEE\nAAA\nSSSS\nEEEEE\nRRRRRR';
-    var expected = 'TEASER\n EASER\n  ASER\n   SER\n    ER\n     R';
+    var input = ['T', 'EE', 'AAA', 'SSSS', 'EEEEE', 'RRRRRR'];
+    var expected = ['TEASER', ' EASER', '  ASER', '   SER', '    ER', '     R'];
     expect(transpose(input)).toEqual(expected);
   });
+
   xit('test many lines', function () {
-    var input = 'Chor. Two households, both alike in dignity,\nIn fair Verona, where we lay our scene,\nFrom ancient grudge break to new mutiny,\nWhere civil blood makes civil hands unclean.\nFrom forth the fatal loins of these two foes\nA pair of star-cross\'d lovers take their life;\nWhose misadventur\'d piteous overthrows\nDoth with their death bury their parents\' strife.\nThe fearful passage of their death-mark\'d love,\nAnd the continuance of their parents\' rage,\nWhich, but their children\'s end, naught could remove,\nIs now the two hours\' traffic of our stage;\nThe which if you with patient ears attend,\nWhat here shall miss, our toil shall strive to mend.';
-    var expected = "CIFWFAWDTAWITW\nhnrhr hohnhshh\no oeopotedi ea\nrfmrmash  cn t\n.a e ie fthow \n ia fr weh,whh\nTrnco miae  ie\nw ciroitr btcr\noVivtfshfcuhhe\n eeih a uote  \nhrnl sdtln  is\noot ttvh tttfh\nun bhaeepihw a\nsaglernianeoyl\ne,ro -trsui ol\nh uofcu sarhu \nowddarrdan o m\nlhg to'egccuwi\ndeemasdaeehris\nsr als t  ists\n,ebk 'phool'h,\n  reldi ffd   \nbweso tb  rtpo\noea ileutterau\nt kcnoorhhnatr\nhl isvuyee'fi \n atv es iisfet\nayoior trr ino\nl  lfsoh  ecti\nion   vedpn  l\nkuehtteieadoe \nerwaharrar,fas\n   nekt te  rh\nismdsehphnnosa\nncuse ra-tau l\n et  tormsural\ndniuthwea'g t \niennwesnr hsts\ng,ycoitkrttet\nn,l rs'a anr\nief 'dgcgdi\ntaol  eoe,v\nyneisl,u;e\n,.sftol \n     ervdt\n     ;ie o\n       f,r \n       eem\n       .me\n          on\n          vd\n          e.\n          ,";
+    var input = ['Chor. Two households, both alike in dignity,', 'In fair Verona, where we lay our scene,', 'From ancient grudge break to new mutiny,', 'Where civil blood makes civil hands unclean.', 'From forth the fatal loins of these two foes', 'A pair of star-cross\'d lovers take their life;', 'Whose misadventur\'d piteous overthrows', 'Doth with their death bury their parents\' strife.', 'The fearful passage of their death-mark\'d love,', 'And the continuance of their parents\' rage,', 'Which, but their children\'s end, naught could remove,', 'Is now the two hours\' traffic of our stage;', 'The which if you with patient ears attend,', 'What here shall miss, our toil shall strive to mend.'];
+    var expected = ['CIFWFAWDTAWITW', 'hnrhr hohnhshh', 'o oeopotedi ea', 'rfmrmash  cn t', '.a e ie fthow ', ' ia fr weh,whh', 'Trnco miae  ie', 'w ciroitr btcr', 'oVivtfshfcuhhe', ' eeih a uote  ', 'hrnl sdtln  is', 'oot ttvh tttfh', 'un bhaeepihw a', 'saglernianeoyl', 'e,ro -trsui ol', 'h uofcu sarhu ', 'owddarrdan o m', 'lhg to\'egccuwi', 'deemasdaeehris', 'sr als t  ists', ',ebk \'phool\'h,', '  reldi ffd   ', 'bweso tb  rtpo', 'oea ileutterau', 't kcnoorhhnatr', 'hl isvuyee\'fi ', ' atv es iisfet', 'ayoior trr ino', 'l  lfsoh  ecti', 'ion   vedpn  l', 'kuehtteieadoe ', 'erwaharrar,fas', '   nekt te  rh', 'ismdsehphnnosa', 'ncuse ra-tau l', ' et  tormsural', 'dniuthwea\'g t ', 'iennwesnr hsts', 'g,ycoitkrttet', 'n,l rs\'a anr', 'ief \'dgcgdi', 'taol  eoe,v', 'yneisl,u;e', ',.sftol ', '     ervdt', '     ;ie o', '       f,r ', '       eem', '       .me', '          on', '          vd', '          e.', '          ,'];
     expect(transpose(input)).toEqual(expected);
   });
 });
