d6c0bd8e93ed5d9c330c5fb094803682f09c299b
Lougenia <lougenia.bailey@gmail.com>
2017-10-29 03:21:58 -0400

    Fixed lint issues in beer song example

--- exercises/beer-song/example.js
+++ exercises/beer-song/example.js
@@ -15,39 +15,43 @@
     return str;
   }
 
-  function action(current_verse) {
-    var sbj, str = '';
+  function action(currentVerse) {
+    var sbj;
+    var str = '';
 
-    if (current_verse === 0) {
+    if (currentVerse === 0) {
       str = 'Go to the store and buy some more, ';
     } else {
-      sbj = (current_verse === 1 ? 'it' : 'one');
+      sbj = (currentVerse === 1 ? 'it' : 'one');
       str = 'Take ' + sbj + ' down and pass it around, ';
     }
 
     return str;
   }
 
-  function next_bottle(current_verse) {
-    return bottles(next_verse(current_verse)).toLowerCase() + ' of beer on the wall.\n';
+  function nextBottle(currentVerse) {
+    return bottles(nextVerse(currentVerse)).toLowerCase() + ' of beer on the wall.\n';
   }
 
-  function next_verse(current_verse) {
-    return current_verse === 0 ? 99 : (current_verse - 1);
+  function nextVerse(currentVerse) {
+    return currentVerse === 0 ? 99 : (currentVerse - 1);
   }
 
   function BeerSong() {}
 
   BeerSong.prototype.sing = function (first, last) {
-    if (typeof (first) === 'undefined') {
-      first = 99;
+    var firstVerseBottleCount = first;
+    var lastVerseBottleCount = last;
+
+    if (typeof (firstVerseBottleCount) === 'undefined') {
+      firstVerseBottleCount = 99;
     }
-    if (typeof (last) === 'undefined') {
-      last = 0;
+    if (typeof (lastVerseBottleCount) === 'undefined') {
+      lastVerseBottleCount = 0;
     }
 
     var verses = [];
-    for (var i = first; i >= last; i--) {
+    for (var i = firstVerseBottleCount; i >= lastVerseBottleCount; i--) {
       verses.push(this.verse(i));
     }
 
@@ -58,7 +62,7 @@
     var line1 = bottles(number) + ' of beer on the wall, ';
     var line2 = bottles(number).toLowerCase() + ' of beer.\n';
     var line3 = action(number);
-    var line4 = next_bottle(number);
+    var line4 = nextBottle(number);
 
     return [line1, line2, line3, line4].join('');
   };
