54eb6d2eef1ac8f343521d41af925aecfc4f024f
Lougenia <lougenia.bailey@gmail.com>
2017-10-31 09:43:28 -0400

    Fixed constructor tests

--- exercises/diffie-hellman/diffie-hellman.spec.js
+++ exercises/diffie-hellman/diffie-hellman.spec.js
@@ -13,13 +13,15 @@ describe('diffie-hellman', function () {
 
   it('throws an error if the constructor arguments are out of range', function () {
     expect(function () {
-      return new DiffieHellman(0, 9999);
+      var dH = new DiffieHellman(0, 9999);
+      return dH;
     }).toThrow();
   });
 
   xit('throws an error if the constructor arguments are not prime', function () {
     expect(function () {
-      return new DiffieHellman(10, 13);
+      var dH = new DiffieHellman(10, 13);
+      return dH;
     }).toThrow();
   });
 
