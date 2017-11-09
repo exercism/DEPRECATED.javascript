ddfea3e351595fd4421504d1b4849d0d14410b5a
Radix16 <12012148+Radix16@users.noreply.github.com>
2017-11-09 08:29:17 -0800

    fix grains exercise

--- exercises/grains/example.js
+++ exercises/grains/example.js
@@ -4,7 +4,7 @@ var BigInt = require('./big-integer');
  * @author github.com/nonsensery
  * @class Grains
  *
- * Computes the number of grains on the squares of a
+ * @classdesc Computes the number of grains on the squares of a
  * chess board, starting with one grain on the first
  * square, and doubling with each successive square.
  */
@@ -14,16 +14,19 @@ function Grains() {
 
 /**
  * Gets the number of grains on the nth square.
+ * @param {number} num - the value to square
+ * @return {number} the square of num
  */
 Grains.prototype.square = function (num) {
-  return BigInt(2).pow(num - 1).toString();
+  return new BigInt(2).pow(num - 1).toString();
 };
 
 /**
  * Gets the total number of grains on all squares.
+ * @return {string} the total
  */
 Grains.prototype.total = function () {
-  var total = BigInt(0);
+  var total = new BigInt(0);
 
   for (var squareNum = 1; squareNum <= 64; ++squareNum) {
     total = total.add(this.square(squareNum));
