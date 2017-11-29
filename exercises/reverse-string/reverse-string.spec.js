585a0c3e180587778373c3d67afcafb64d469594
eliaahadi <elia.ahadi@gmail.com>
2017-11-30 09:06:02 +1100

    fixed array inputs

--- exercises/reverse-string/reverse-string.spec.js
+++ exercises/reverse-string/reverse-string.spec.js
@@ -3,37 +3,37 @@ var ReverseString = require('./reverse-string');
 describe('ReverseString', function () {
   it('empty string', function () {
     var subject = new ReverseString('');
-    var matches = subject.matches([ '']);
+    var matches = subject.matches('');
 
-    expect(matches).toEqual(['']);
+    expect(matches).toEqual('');
   });
 
   xit('a word', function () {
     var subject = new ReverseString('robot');
-    var matches = subject.matches(['tobor']);
+    var matches = subject.matches('tobor');
 
-    expect(matches).toEqual(['tobor']);
+    expect(matches).toEqual('tobor');
   });
 
   xit('a capitalized word', function () {
     var subject = new ReverseString('Ramen');
-    var matches = subject.matches(['nemaR']);
+    var matches = subject.matches('nemaR');
 
-    expect(matches).toEqual(['nemaR']);
+    expect(matches).toEqual('nemaR');
   });
 
   xit('a sentence with punctuation', function () {
     var subject = new ReverseString('I am hungry!');
-    var matches = subject.matches(['!yrgnuh ma I']);
+    var matches = subject.matches('!yrgnuh ma I');
 
-    expect(matches).toEqual(['!yrgnuh ma I']);
+    expect(matches).toEqual('!yrgnuh ma I');
   });
 
   xit('a palindrome', function () {
     var subject = new ReverseString('racecar');
     var matches = subject.matches('racecar');
 
-    expect(matches).toEqual(['racecar']);
+    expect(matches).toEqual('racecar');
   });
 
 });
