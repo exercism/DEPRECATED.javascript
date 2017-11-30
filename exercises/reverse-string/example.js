808cf1131e6343a5149316b3d421a0e0c400f839
eliaahadi <elia.ahadi@gmail.com>
2017-12-01 08:08:42 +1100

    spacing updates #3

--- exercises/reverse-string/example.js
+++ exercises/reverse-string/example.js
@@ -4,10 +4,10 @@ function ReverseString(string) {
   this.string = string;
 }
 
-ReverseString.prototype.matches=function(string){
+ReverseString.prototype.matches = function (string) {
   var revString = '';
-  for (var i=string.length-1; i>=0; i--){
-   revString += string[i];
+  for (var i = string.length-1; i >= 0; i--) {
+    revString += string[i];
   }
   return revString;
 };
