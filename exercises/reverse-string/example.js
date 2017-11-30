1a390ddc5f5000818cc665876dc1d76b7520767f
eliaahadi <elia.ahadi@gmail.com>
2017-12-01 08:03:39 +1100

    spacing updates #2

--- exercises/reverse-string/example.js
+++ exercises/reverse-string/example.js
@@ -1,14 +1,13 @@
 'use strict';
 
 function ReverseString(string) {
-	this.string = string;
+  this.string = string;
 }
 
-ReverseString.prototype.matches = function (string) {
-	
+ReverseString.prototype.matches=function(string){
   var revString = '';
   for (var i=string.length-1; i>=0; i--){
-  	revString += string[i];
+   revString += string[i];
   }
   return revString;
 };
