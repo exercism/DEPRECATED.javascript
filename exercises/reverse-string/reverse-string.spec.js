96e1ec4471e41b914c527be028efee0c75cb2463
eliaahadi <elia.ahadi@gmail.com>
2017-12-25 23:05:39 +1100

    updated spec and example files v3

--- exercises/reverse-string/reverse-string.spec.js
+++ exercises/reverse-string/reverse-string.spec.js
@@ -4,28 +4,29 @@ describe('ReverseString', function () {
   it('empty string', function () {
     var expected = '';
     var actual = reverseString('');
-    expect(actual).toEqual(expected);  
+    expect(actual).toEqual(expected);
   });
+  
   it('a word', function () {
-    var expected = 'tac';  
-    var actual = reverseString('cat');
+    var expected = 'tobor';
+    var actual = reverseString('robot');
     expect(actual).toEqual(expected);
   });
 
   it('a capitalized word', function () {
-    var expected = 'nemaR';      
+    var expected = 'nemaR';
     var actual = reverseString('Ramen');
     expect(actual).toEqual(expected);
   });
 
   it('a sentence with punctuation', function () {
-    var expected = '!yrgnuh ma I';            
+    var expected = '!yrgnuh ma I';           
     var actual = reverseString('I am hungry!');
-    expect(actual).toEqual(expected); 
+    expect(actual).toEqual(expected);
   });
 
   it('a palindrome', function () {
-    var expected = 'racecar';    
+    var expected = 'racecar';   
     var actual = reverseString('racecar');
     expect(actual).toEqual(expected);
   });
