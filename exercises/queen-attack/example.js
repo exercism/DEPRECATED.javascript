d6b7078d6478ba5d99437a8de1796a36ec873a8a
Susan Lippa <sfl@Susans-MacBook-Pro.local>
2018-03-03 11:05:20 -0600

    queen-attack linting

--- exercises/queen-attack/example.js
+++ exercises/queen-attack/example.js
@@ -1,12 +1,9 @@
 'use strict';
 
 
-module.exports = function (options) {
-  if (options === 'undefined') {
-    options.white = [0, 3];
-    options.black = [7, 3];
-  }
-
+module.exports = function (passedInOptions) {
+  var options = {};
+  options = passedInOptions || {white: [0, 3], black: [7, 3]};
 
   if (options.white[0] === options.black[0] && options.white[1] === options.black[1]) {
     throw String('Queens cannot share the same space');
@@ -64,3 +61,4 @@ module.exports = function (options) {
     return this.boardRepresentation();
   };
 };
+
