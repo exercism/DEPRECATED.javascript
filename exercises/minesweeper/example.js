2782f1f88cbdd5c7098b6a49679fa2b763fb84b3
tarunvelli <tarunvelli@gmail.com>
2018-03-10 15:03:10 +0530

    fix linting for minesweeper exercise

--- exercises/minesweeper/example.js
+++ exercises/minesweeper/example.js
@@ -21,25 +21,25 @@ Minesweeper.prototype.annotate = function (rows) {
   }
   var board = rows.map(function (row) { return row.split(''); });
   var outBoard = [];
-  for (var x = 0; x < board.length; x++) {
+  board.forEach(function (memberX, x)  {
     outBoard[x] = [];
-    for (var y = 0; y < board[x].length; y++) {
-      var spot = board[x][y];
+    memberX.forEach(function (memberXY, y) {
+      var spot = memberXY;
       if (spot === '*') {
         outBoard[x][y] = spot;
-        continue;
+      } else {
+        var bombCount = this.distanceXdistanceYs.map(function (dxdy) {
+          if (typeof board[x + dxdy[0]] === 'undefined') {
+            return 0;
+          }
+          return board[x + dxdy[0]][y + dxdy[1]] === '*' ? 1 : 0;
+        }).reduce(function (total, num) {
+          return total + num;
+        });
+        outBoard[x][y] = bombCount > 0 ? bombCount : ' ';
       }
-      var bombCount = this.distanceXdistanceYs.map(function (dxdy) {
-        if (board[x + dxdy[0]] === undefined) {
-          return 0;
-        }
-        return board[x + dxdy[0]][y + dxdy[1]] === '*' ? 1 : 0;
-      }).reduce(function (total, num) {
-        return total + num;
-      });
-      outBoard[x][y] = bombCount > 0 ? bombCount : ' ';
-    }
-  }
+    }, this);
+  }, this);
   return outBoard.map(function (row) {
     return row.join('');
   });
